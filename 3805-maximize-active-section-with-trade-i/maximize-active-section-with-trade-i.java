cda2c0a66ac9dc0feea6aef4ea4ea51c93f8b30d
abhishekkanade <15abhishek2005@gmail.com>
2026-07-21 17:49:45 +0530

    Time: 54 ms (92.96%) | Memory: 48.1 MB (39.44%) - LeetSync

--- 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.java
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.java
@@ -1,28 +1,22 @@
 class Solution {
     public int maxActiveSectionsAfterTrade(String s) {
         
-        int[] arr = new int[s.length()];
-        int n=arr.length;
+
 
         int i=0;
-        for(char c: s.toCharArray()){
-            arr[i] = c-'0';
-            i++;
-        }
-
-        i=0;
+        int n=s.length();
         int max0s=0;
         int ones =0;
         int prevrun =-1;
 
         while(i<n){
-            if(arr[i]==1){
+            if(s.charAt(i)=='1'){
                 ones++;
                 i++;
             } 
             else{
                 int curr0s=0;
-                while(i<n && arr[i]==0){
+                while(i<n && s.charAt(i)=='0'){
                     curr0s++;
                     i++;
                 }
