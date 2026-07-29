65cc5a29938adc6185179bf409e9d67777844536
abhishekkanade <15abhishek2005@gmail.com>
2026-07-30 01:40:41 +0530

    Time: 1 ms (99.93%) | Memory: 46.9 MB (28.94%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,23 +10,44 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
+
+// OPTIMAL - BINARY SEARCH
         if(lists.length==0) return null;
-        if(lists.length==1) return lists[0];
-        ArrayList<ListNode> arrlist = new ArrayList<>();
-        for(ListNode sll : lists){
-            arrlist.add(sll);
-        }
 
-        while(arrlist.size()>1){
-            ListNode list1 = arrlist.remove(arrlist.size()-2);
-            ListNode list2 = arrlist.remove(arrlist.size()-1);
+        return divide(lists, 0, lists.length-1);
+
+
+// BRUTE FORCE 
+        // if(lists.length==0) return null;
+        // if(lists.length==1) return lists[0];
+        // ArrayList<ListNode> arrlist = new ArrayList<>();
+        // for(ListNode sll : lists){
+        //     arrlist.add(sll);
+        // }
+
+        // while(arrlist.size()>1){
+        //     ListNode list1 = arrlist.remove(arrlist.size()-2);
+        //     ListNode list2 = arrlist.remove(arrlist.size()-1);
             
-            ListNode newlist = merge(list1, list2);
-            arrlist.add(newlist);
-        }
+        //     ListNode newlist = merge(list1, list2);
+        //     arrlist.add(newlist);
+        // }
 
-        return arrlist.get(0);
+        // return arrlist.get(0);
     }
+
+
+    ListNode divide(ListNode[] lists, int low, int high){
+        if(low==high) return lists[low];
+
+        int mid = low+(high-low)/2;
+
+        ListNode temp1 = divide(lists, low, mid);
+        ListNode temp2 = divide(lists, mid+1, high);
+
+        return merge(temp1, temp2);
+    }
+
 
     ListNode merge(ListNode list1, ListNode list2){
         ListNode t1 = list1;
