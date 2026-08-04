eaa888d959113ba6cd3f5afa0fe305c613a0fbbf
abhishekkanade <15abhishek2005@gmail.com>
2026-08-04 23:32:04 +0530

    Time: 10 ms (6.99%) | Memory: 52.2 MB (5.33%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.java
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -1,14 +1,40 @@
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
-        Arrays.sort(nums);
-        ArrayList<Integer> ans=new ArrayList<>();
-        for(int i=1; i<nums.length; i++){
-            while(nums[i]-1 != nums[i-1]) {
-                ans.add(nums[i-1]+1);
-                nums[i-1] +=1;
 
-            }
+        int min=Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i<nums.length; i++){
+            min=Math.min(min, nums[i]);
+            max=Math.max(max, nums[i]);
+        }
+        System.out.println(min+" "+max);
+
+        boolean[] flag = new boolean[max-min-1];
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==min || nums[i]==max) continue;
+            flag[nums[i]-min-1]=true;
+
+        }
+
+        ArrayList<Integer> ans=new ArrayList<>();
+        for(int i=0; i<flag.length; i++){
+            if(!flag[i]) ans.add(i+min+1);
+
         }
         return ans;
+
+
+// T.C. O(nlogn) , S.C.  O(1)         
+        // Arrays.sort(nums);
+        // ArrayList<Integer> ans=new ArrayList<>();
+        // for(int i=1; i<nums.length; i++){
+        //     while(nums[i]-1 != nums[i-1]) {
+        //         ans.add(nums[i-1]+1);
+        //         nums[i-1] +=1;
+
+        //     }
+        // }
+        // return ans;
     }
 }
